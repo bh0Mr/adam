d3b855b09148c6a4bb9c8dba556716ff200b3fad
atul <atulkumar5301@gmail.com>
2023-08-03 23:52:34 +0530

    login pages

---
+++ main.js
@@ -0,0 +1,32 @@
+
+
+   console.log("Hello world" );
+    //alert("me")
+    document.write("This is document");
+    console.warn("This is warning");
+    
+    var num1 = 33;
+    var num2 =  77;
+    console.log(num1 + num2);
+    var cars = {
+        BMW:55,
+        maruti:11,
+        tampo:22
+        
+    }
+    console.log(cars);
+var a = 4;
+var b = 5;
+var c = 4
+if (a == c);
+console.log(a);
+if (a==b);
+console.log(b);
+var un;
+console.log(un)
+
+function showAlert() {
+    alert('Hello! This is an alert popup.');
+}
+
+
